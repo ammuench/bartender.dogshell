ba95feb31e7ae4a63dfe4f75c0a36a7254312125
Andrey <andrey@laptop>
2022-07-10 15:59:28 +0100

    From pet names to cocktails

--- app.py
+++ app.py
@@ -1,35 +1,41 @@
 import os
-
+import logging
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+logging.basicConfig(filename='logfile.log', level=logging.INFO)
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        cocktail = request.form["cocktail"]
+        app.logger.info(cocktail)
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt_cocktails(cocktail),
+            temperature=0.1,
+            max_tokens=300
         )
+        app.logger.info(response)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt_cocktails(coctail_name: str):
+    return """Suggest long descriptive recipe for a cocktail.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+Cocktail: Margarita
+Recipe: 1 part Cazadores Tequila, ½ part triple sec liqueur, ½ part lime juice, 1 lime wedge, Salt, Cubed ice. Chill your glass (the easiest way is to fill it with ice). Put lots of ice and all of the ingredients into a shaker, then shake hard for about 30 seconds to chill the liquid really well. Run a lime wedge around the outside of the rim of your glass before rolling the rim in salt. Double strain the mix into the glass.
+Cocktail: Cosmopolitan
+Recipe: 45ml lemon vodka, 15ml triple sec, 30ml cranberry juice, 10ml lime juice, ice. For the garnish orange zest, or a lime wedge on the rim of the glass. Shake ingredients in a cocktail shaker with ice and strain into a cocktail glass. To make the garnish: hold a 3cm round piece of orange zest about 10cm above your cosmo and very carefully wave it over a lit match or lighter flame. Bend the outer edge of the zest in towards the flame so that the orange oils are released, then drop the zest into your drink.
+Cocktail: Daiquiri
+Recipe: 50ml white rum, 25ml lime juice, 10ml sugar syrup, ice. Shake all the ingredients in a cocktail shaker and strain into a cocktail glass.
+Cocktail: Gimlet
+Recipe: 2 1/2 ounces gin, 1/2 ounce lime juice, freshly squeezed, 1/2 ounce simple syrup, lime wheel. Add the gin, lime juice and simple syrup to a shaker with ice and shake until well-chilled. Strain into a chilled cocktail glass or an rocks glass filled with fresh ice. Garnish with a lime wheel.
+Cocktail: {}
+Recipe:""".format(coctail_name.capitalize())
