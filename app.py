924e0bad35018bc4130c048566471f31a03c2943
Andrey <andrey@laptop>
2022-07-10 18:26:58 +0100

    Stranger recipes

--- app.py
+++ app.py
@@ -13,21 +13,33 @@ def index():
     if request.method == "POST":
         cocktail = request.form["cocktail"]
         app.logger.info(cocktail)
-        response = openai.Completion.create(
+        response_normal = openai.Completion.create(
             model="text-davinci-002",
             prompt=generate_prompt_cocktails(cocktail),
-            temperature=0.1,
-            max_tokens=300
+            temperature=0.75,
+            top_p=0.75,
+            max_tokens=150
         )
-        app.logger.info(response)
-        return redirect(url_for("index", result=response.choices[0].text))
+        app.logger.info("Normal recipe:")
+        app.logger.info(response_normal)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        response_absurd = openai.Completion.create(
+            model="text-babbage-001",
+            prompt=generate_prompt_cocktails(cocktail),
+            temperature=1.5,
+            top_p=0.9,
+            max_tokens=150,
+            # stop="Cocktail:"
+        )
+        app.logger.info("Absurd recipe:")
+        app.logger.info(response_absurd)
+        return redirect(url_for("index", result_normal=response_normal.choices[0].text, cocktail=cocktail, result_absurd=response_absurd.choices[0].text))
+
+    return render_template("index.html", result_normal=request.args.get("result_normal"), result_absurd=request.args.get("result_absurd"), cocktail=request.args.get("cocktail"))
 
 
-def generate_prompt_cocktails(coctail_name: str):
-    return """Suggest long descriptive recipe for a cocktail.
+def generate_prompt_cocktails(cocktail_name: str):
+    return """Suggest recipe for a cocktail.
 
 Cocktail: Margarita
 Recipe: 1 part Cazadores Tequila, ½ part triple sec liqueur, ½ part lime juice, 1 lime wedge, Salt, Cubed ice. Chill your glass (the easiest way is to fill it with ice). Put lots of ice and all of the ingredients into a shaker, then shake hard for about 30 seconds to chill the liquid really well. Run a lime wedge around the outside of the rim of your glass before rolling the rim in salt. Double strain the mix into the glass.
@@ -38,4 +50,4 @@ Recipe: 50ml white rum, 25ml lime juice, 10ml sugar syrup, ice. Shake all the in
 Cocktail: Gimlet
 Recipe: 2 1/2 ounces gin, 1/2 ounce lime juice, freshly squeezed, 1/2 ounce simple syrup, lime wheel. Add the gin, lime juice and simple syrup to a shaker with ice and shake until well-chilled. Strain into a chilled cocktail glass or an rocks glass filled with fresh ice. Garnish with a lime wheel.
 Cocktail: {}
-Recipe:""".format(coctail_name.capitalize())
+Recipe:""".format(cocktail_name.capitalize())
